b0137624339e209ba3d4ca914be169b51332304b
Teurowel <teurowel@gmail.com>
2020-09-30 00:53:21 +0900

    Commit for Lab2 Responsive Layout

---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameController : MonoBehaviour
+{
+    //public TMP_Text sceneLabel;
+    public TMP_Text LivesLabel;
+    public TMP_Text ScoreLabel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Detect device orientation
+        //if(Input.deviceOrientation == DeviceOrientation.LandscapeLeft)
+        //{
+        //    Debug.Log("Landscape left");
+        //}
+
+        //if (Input.deviceOrientation == DeviceOrientation.LandscapeRight)
+        //{
+        //    Debug.Log("Landscape right");
+        //}
+
+        //if (Input.deviceOrientation == DeviceOrientation.Portrait)
+        //{
+        //    Debug.Log("Portrait");
+        //}
+
+        switch(Input.deviceOrientation)
+        {
+            case DeviceOrientation.LandscapeLeft:
+                //sceneLabel.text = "Landscape left";
+                LivesLabel.rectTransform.anchoredPosition = new Vector2(370f, -105f);
+                ScoreLabel.rectTransform.anchoredPosition = new Vector2(-349f, -105f);
+                break;
+
+            case DeviceOrientation.LandscapeRight:
+                //sceneLabel.text = "Landscape right";
+                LivesLabel.rectTransform.anchoredPosition = new Vector2(248f, -105f);
+                ScoreLabel.rectTransform.anchoredPosition = new Vector2(-490f, -105f);
+                break;
+
+            case DeviceOrientation.Portrait:
+                //sceneLabel.text = "portriat";
+                LivesLabel.rectTransform.anchoredPosition = new Vector2(248f, -185f);
+                ScoreLabel.rectTransform.anchoredPosition = new Vector2(-349f, -185f);
+                break;
+
+            case DeviceOrientation.Unknown:
+                //sceneLabel.text = "Unknown orientation";
+                break;
+        }
+    }
+
+  
+}
---
+++ Assets/_Scripts/TestButtonBehaviour.cs
@@ -0,0 +1,34 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class TestButtonBehaviour : MonoBehaviour
+{
+    //public TMP_Text sceneLabel;
+    public TMP_Text LivesLabel;
+    public TMP_Text ScoreLabel;
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+
+    public void OnTestButtonPressed()
+    {
+        Debug.Log("TestButton Pressed");
+        //SceneManager.LoadScene("End");
+        //sceneLabel.text = "test";
+        LivesLabel.rectTransform.position = new Vector3(370f, -105f, 0.0f);
+        //ScoreLabel.rectTransform.position = new Vector3(-349f, -105f, 0.0f);
+        ScoreLabel.rectTransform.anchoredPosition = new Vector3(-349f, -105f, 0.0f);
+    }
+}
